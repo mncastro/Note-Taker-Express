b42b203ef80d279a630577ee66595db8d10ce7e4
¨Mario Castro¨ <mnc634@utexas.edu>
2021-02-28 15:52:02 -0600

    added script for notes data

---
+++ db/notes.js
@@ -0,0 +1,11 @@
+// Input notes data in array and export data for saving
+
+const notesData = [
+    {
+        title: "Chapter summary",
+        text: "Write report summary of chapter 5",
+        id: "0005"
+    },
+];
+
+module.exports = notesData;
