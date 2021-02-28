a6e6b790419886b22ed08f49e9e2d2b38b3fdd73
¨Mario Castro¨ <mnc634@utexas.edu>
2021-02-28 16:17:11 -0600

    added html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,57 +1,25 @@
-// Variables for dependencies and routes for importing data
+// Variables for dependencies and routes
 
-const db = require("../db/db.json");
+const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const notesData = require("../db/notes");
 
-// HTTP API requests scripts
 
-// GET route
+// HTML routes to front end
 
 module.exports = function (app) {
-    app.get("/api/notes", (req, res) => {
-        fs.readFile("./db/db.json", (err, notesData) => {
-            if (err) throw err;
-            res.json(JSON.parse(notesData));
-        });
+    app.use(express.static(path.join(__dirname, "../public")));
+
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
-
-    // POST request
-
-    app.post("/api/notes", (req, res) => {
-        fs.readFile("./db/db.json", (err, data) => {
-            if (err) throw err;
-            const notesData = JSON.parse(data);
-            notesData.push(req.body);
-            console.log(req.body);
-            for (let i = 0; i < notesData.length; i++) {
-                notesData[i].id = i + 1;
-            } // Turn data into string and push into database
-            fs.writeFile("./db/db.json", JSON.stringify(notesData), (err) => {
-                if (err) throw err;
-                res.send(db);
-            });
-        });
+    // If no data, sends user to index
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    // DELETE request
-    app.delete("/api/notes/:id", (req, res) => {
-        fs.readFile("./db/db.json", (err, data) => {
-            if (err) throw err;
-            let newNote = JSON.parse(data);
-            for (let i = 0; i < notesData.length; i++) {
-                if (newNote[i].id == req.params.id{
-                    newNote.splice(i, 1);
-                }
-            }
-
-            // Write front end and export note from database
-            fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
-                if (err) throw err;
-                res.send(newNote);
-            });
-        });
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+
 };
